6bb4003b36d069e8b0c50c5047094b27d3ed35c0
Walrus <buttermunchinwalrus@gmail.com>
2025-01-09 17:27:37 -0500

    First push

---
+++ SquareCS/src/Program.cs
@@ -0,0 +1,6 @@
+﻿Square noobSqaure = new Square(10);
+Square squareNoob = new Square();
+
+Square.DrawStairs(squareNoob.GetSize());
+
+Console.WriteLine("Hello please select your square size");
---
+++ SquareCS/src/Square.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace SquareCS.src
+//{
+class Square
+{
+
+    private int size;
+
+   
+    public Square(int data)
+    {
+        SetSize(data);
+    }
+
+    public Square()
+    {
+        SetSize(0);
+    }
+
+    
+    public int GetSize()
+    {
+        return size;
+    }
+
+    public void SetSize(int setter)
+    {
+        size = setter;
+    }
+
+    public static void Print(int data)
+    {
+        Console.WriteLine(data);
+    }
+
+    public static void DrawStairs(int data)
+    {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < i; j++)
+            {
+                Console.Write(data);
+            }
+            Console.WriteLine();
+        }
+
+    }
+    }
+//}
